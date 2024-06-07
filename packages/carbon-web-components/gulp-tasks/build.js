ce2a99af5ad92d3c02717bda3a47fadc8cdcacdb
Matthew Oliveira <m4olivei@gmail.com>
2024-06-07 11:06:31 -0400

    feat(build): add build:no-dist command for cwc and c4ibm

--- packages/carbon-web-components/gulp-tasks/build.js
+++ packages/carbon-web-components/gulp-tasks/build.js
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2022
+ * Copyright IBM Corp. 2020, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -20,4 +20,9 @@ gulp.task(
     gulp.task('build:sass'),
     gulp.task('build:dist')
   )
+);
+
+gulp.task(
+  'build:no-dist',
+  gulp.parallel(gulp.task('build:modules'), gulp.task('build:sass'))
 );
--- packages/web-components/gulp-tasks/build.js
+++ packages/web-components/gulp-tasks/build.js
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2022
+ * Copyright IBM Corp. 2020, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -26,5 +26,13 @@ gulp.task(
       gulp.task('build:modules'),
       gulp.task('build:sass')
     )
+  )
+);
+
+gulp.task(
+  'build:no-dist',
+  gulp.series(
+    gulp.task('vendor'),
+    gulp.parallel(gulp.task('build:modules'), gulp.task('build:sass'))
   )
 );
